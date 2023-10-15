73aa6ee3d3aa48b5a7501138d747fedc6fc0a411
Ogunderu Noah Ife <ifeoluwadayonoah@gmail.com>
2023-10-15 07:43:32 +0100

    initial work page with project cards and hero section

--- src/routes/about/about.jsx
+++ src/routes/about/about.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const About = () => {
   return (
-    <div>About</div>
+    <div className='space-y-56 max-w-screen-2xl flex items-center justify-center flex-col pt-28 text-nao-white'>
+      Aboutooooo
+    </div>
   )
 }
--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { Hero } from './hero/hero'
+import { SelectedWork } from './selectedwork/selectedwork'
 
 export const Home = () => {
+
   return (
-    <div>home</div>
+    <main className='space-y-56 max-w-screen-2xl flex items-center justify-center flex-col pt-14'>
+      <Hero />
+      <SelectedWork />
+    </main>
   )
 }
